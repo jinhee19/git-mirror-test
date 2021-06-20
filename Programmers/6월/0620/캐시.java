45602335cbee40917df05ba0907443ad36c6048a
jinhee19 <sinnanjini@gmail.com>
2021-06-20 23:37:39 +0900

    add 0620

---
+++ Programmers/6월/0620/캐시.java
@@ -0,0 +1,12 @@
+package programmers.june;
+
+public class 캐시 {
+	static int cacheSize;
+	static String[] cities;
+	
+	public static void main(String[] args) {
+		int answer = 0;
+		
+		System.out.println(answer);
+	}
+}
---
+++ Programmers/6월/0620/프렌즈4블록.java
@@ -0,0 +1,13 @@
+package programmers.june;
+
+public class 프렌즈4블록 {
+	static int m = 4;
+	static int n = 5;
+	static String[] board = {"CCBDE", "AAADE", "AAABF", "CCBBF"};
+	
+	public static void main(String[] args) {
+		int answer = 0;
+		
+		System.out.println(answer);
+	}
+}
