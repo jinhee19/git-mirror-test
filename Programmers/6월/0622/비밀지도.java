d959bca3e34ee698fdf656b6d6b901dd802fb17f
jinhee19 <sinnanjini@gmail.com>
2021-06-22 23:30:33 +0900

    add 0622

---
+++ Programmers/6월/0622/비밀지도.java
@@ -0,0 +1,16 @@
+package programmers.june;
+
+public class 비밀지도 {	
+	static int n;
+	static int[] arr1;
+	static int[] arr2;
+	
+	
+	public static void main(String[] args) {
+		String[] answer = {};
+		
+		for(int i = 0; i < answer.length; i++) {
+			System.out.print(answer[i] + " ");
+		}
+	}
+}
