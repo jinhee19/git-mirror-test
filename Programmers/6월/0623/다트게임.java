8a9613165ae2940ec1d95e8b81793551b5774c19
jinhee19 <sinnanjini@gmail.com>
2021-06-23 23:33:36 +0900

    add 0623

---
+++ Programmers/6월/0623/다트게임.java
@@ -0,0 +1,11 @@
+package programmers.june;
+
+public class 다트게임 {
+	static String dartResult;
+	
+	public static void main(String[] args) {
+		int answer = 0;
+		
+		System.out.println(answer);
+	}
+}
