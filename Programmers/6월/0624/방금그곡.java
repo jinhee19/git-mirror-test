cb075f74555be39dd5b563d65e832b9f9995bcb6
jinhee19 <sinnanjini@gmail.com>
2021-06-24 23:35:21 +0900

    add 0624

---
+++ Programmers/6월/0624/방금그곡.java
@@ -0,0 +1,12 @@
+package programmers.june;
+
+public class 방금그곡 {
+	static String m = "ABCDEFG";
+	static String[] musicinfos = {"12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"};
+	
+	public static void main(String[] args) {
+		String answer = "";
+		
+		System.out.println(answer);
+	}
+}
