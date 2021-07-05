680ca5a9e170947562b3c148e6414e09eb979e65
jinhee19 <sinnanjini@gmail.com>
2021-07-05 23:58:01 +0900

    add 0705

--- Baekjoon/7월/0705/Main_1085_직사각형에서탈출.java
+++ Baekjoon/7월/0705/Main_1085_직사각형에서탈출.java
@@ -1,7 +1,49 @@
 package baekjoon.july;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main_1085_직사각형에서탈출 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
+		int w = 0, h = 0, x = 0, y = 0;
+		
+		x = Integer.parseInt(st.nextToken());
+		y = Integer.parseInt(st.nextToken());
+		w = Integer.parseInt(st.nextToken());
+		h = Integer.parseInt(st.nextToken());
+		
+		int midw = w/2;
+		int midh = h/2;
+		
+		int answer = 0;
+		
+		if(x < midw && y < midh) {
+			if(x < y)
+				answer = x;
+			else
+				answer = y;
+		}else if(x < midw && y >= midh) {
+			if(x < (h - y))
+				answer = x;
+			else
+				answer = h-y;
+		}else if(x >= midw && y < midh) {
+			if((w-x) < y)
+				answer = w - x;
+			else
+				answer = y;
+		}else {
+			if((w-x) < (h-y))
+				answer = w - x;
+			else
+				answer = h - y;
+		}
+		
+		System.out.println(answer);
 	}
 }
