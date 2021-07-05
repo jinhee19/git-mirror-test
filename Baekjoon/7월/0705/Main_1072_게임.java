cc536e0a6d05e22d67f652746ae6e52b51ff823e
jinhee19 <sinnanjini@gmail.com>
2021-07-05 23:48:53 +0900

    add 0705

---
+++ Baekjoon/7월/0705/Main_1072_게임.java
@@ -0,0 +1,7 @@
+package baekjoon.july;
+
+public class Main_1072_게임 {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ Baekjoon/7월/0705/Main_1085_직사각형에서탈출.java
@@ -0,0 +1,7 @@
+package baekjoon.july;
+
+public class Main_1085_직사각형에서탈출 {
+	public static void main(String[] args) {
+		
+	}
+}
