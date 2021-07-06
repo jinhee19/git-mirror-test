7dc93c077e5854c0a855f75dd888a788f0b0fe2d
jinhee19 <sinnanjini@gmail.com>
2021-07-06 23:43:23 +0900

    add 0706

---
+++ Baekjoon/7월/0706/Main_11005_진법변환2.java
@@ -0,0 +1,12 @@
+package baekjoon.july;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_11005_진법변환2 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+	}
+}
