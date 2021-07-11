0ed05ccd6dd257f66f101b00c9069f4904290079
jinhee19 <sinnanjini@gmail.com>
2021-07-11 23:54:36 +0900

    add 0711

---
+++ Baekjoon/7월/0711/Main_1072_게임.java
@@ -0,0 +1,25 @@
+package baekjoon.july;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1072_게임 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int X = Integer.parseInt(st.nextToken());
+		int Y = Integer.parseInt(st.nextToken());
+		
+		
+	}
+	
+	
+}
+
+/*
+ * 분류 : 수학, 이분탐색
+ */
+
