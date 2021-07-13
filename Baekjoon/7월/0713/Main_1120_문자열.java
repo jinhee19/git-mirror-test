a05d6b1524ab9987862f5ac712448bb2504058b4
jinhee19 <sinnanjini@gmail.com>
2021-07-13 23:51:09 +0900

    add 0713

---
+++ Baekjoon/7월/0713/Main_1120_문자열.java
@@ -0,0 +1,16 @@
+package baekjoon.july;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1120_¹®ÀÚ¿­ {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		String A = st.nextToken();
+		String B = st.nextToken();
+	}
+}
