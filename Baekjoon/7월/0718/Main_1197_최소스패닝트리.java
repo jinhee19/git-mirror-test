7f47ad5b2c0636f26075abb3ee67af112b63bcd7
jinhee19 <sinnanjini@gmail.com>
2021-07-18 23:59:00 +0900

    add BOJ 1197 최소 스패닝 트리

---
+++ Baekjoon/7월/0718/Main_1197_최소스패닝트리.java
@@ -0,0 +1,14 @@
+package baekjoon.july;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1197_최소스패닝트리 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+	}
+}
