908b3da1305c2b09ea5c9caec267198cf958b13c
jinhee19 <sinnanjini@gmail.com>
2021-07-19 23:52:12 +0900

    add BOJ 1213 팰린드롬 만들기

---
+++ Baekjoon/7월/0719/Main_1213_팰린드롬만들기.java
@@ -0,0 +1,14 @@
+package baekjoon.july;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_1213_팰린드롬만들기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String name = br.readLine();
+		
+		
+	}
+}
