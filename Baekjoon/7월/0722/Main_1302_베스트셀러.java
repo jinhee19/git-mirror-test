f660d32cac9623fda8d9e8652f640017e5854f2f
jinhee19 <sinnanjini@gmail.com>
2021-07-23 00:09:57 +0900

    fix BOJ 1302 베스트셀러

--- Baekjoon/7월/0722/Main_1302_베스트셀러.java
+++ Baekjoon/7월/0722/Main_1302_베스트셀러.java
@@ -3,7 +3,11 @@ package baekjoon.july;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 public class Main_1302_º£½ºÆ®¼¿·¯ {
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -17,6 +21,20 @@ public class Main_1302_º£½ºÆ®¼¿·¯ {
 				map.put(title, 1);
 			map.put(title,  map.get(title)+1);
 		}
+		
+		ArrayList<Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+		
+		Collections.sort(list, new Comparator<Entry<String, Integer>>(){
+
+			@Override
+			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+				if(o1.getValue() == o2.getValue())
+					return o1.getKey().compareTo(o2.getKey());
+				return o2.getValue().compareTo(o1.getValue());
+			}
+		});
+		
+		System.out.println(list.get(0).getKey());
 		
 	}
 }
