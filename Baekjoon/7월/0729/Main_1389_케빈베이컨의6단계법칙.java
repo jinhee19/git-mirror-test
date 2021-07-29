ce782f3c846c7d97d592e1cb6ef63b93900823b1
JINHEE <sinnanjini@gmail.com>
2021-07-29 23:59:58 +0900

    add BOJ 1389 케빈베이컨의 6단계 법칙

---
+++ Baekjoon/7월/0729/Main_1389_케빈베이컨의6단계법칙.java
@@ -0,0 +1,10 @@
+package baekjoon.july;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_1389_케빈베이컨의6단계법칙 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
