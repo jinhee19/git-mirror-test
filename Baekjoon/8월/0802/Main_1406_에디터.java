647a7a04ec6b8d27cf602096cccf66f6b0668467
JINHEE <sinnanjini@gmail.com>
2021-08-02 23:58:35 +0900

    add BOJ 1406 에디터

---
+++ Baekjoon/8월/0802/Main_1406_에디터.java
@@ -0,0 +1,10 @@
+package baekjoon.august;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_1406_¿¡µğÅÍ {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
